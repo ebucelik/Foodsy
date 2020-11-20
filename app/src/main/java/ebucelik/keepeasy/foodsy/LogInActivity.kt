db3f2cdb5d822e122a85a52e466ebcd90409c8cd
Ebu Bekir Celik <ebu.celik@stud.fh-campuswien.ac.at>
2020-11-20 11:26:43 +0100

    Data binding to get more performance.

--- app/src/main/java/ebucelik/keepeasy/foodsy/LogInActivity.kt
+++ app/src/main/java/ebucelik/keepeasy/foodsy/LogInActivity.kt
@@ -1,56 +1,63 @@
 package ebucelik.keepeasy.foodsy
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import ebucelik.keepeasy.foodsy.databinding.ActivityLogInBinding
 
 class LogInActivity : AppCompatActivity() {
 
     //lateinit promises that this variable will be initialized before calling any operations on it.
-    lateinit var passwordTxt: TextInputEditText
-    lateinit var passwordLayout: TextInputLayout
-    lateinit var usernameTxt: TextInputEditText
-    lateinit var usernameLayout: TextInputLayout
+    //lateinit var passwordTxt: TextInputEditText
+
+    //Data Type is the activity name + Binding
+    private lateinit var binding: ActivityLogInBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        //setContentView(R.layout.activity_log_in)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_log_in)
 
-        val loginBtn: MaterialButton = findViewById(R.id.loginBtn)
-
-        loginBtn.setOnClickListener {
+        binding.loginBtn.setOnClickListener {
             checkUsername()
             checkPassword()
         }
-
-        passwordTxt = findViewById(R.id.password)
-        passwordLayout = findViewById(R.id.passwordLayout)
-        usernameTxt = findViewById(R.id.username)
-        usernameLayout = findViewById(R.id.usernameLayout)
     }
 
     private fun checkPassword() {
-        if (passwordTxt.length() == 0){
-            passwordLayout.isHelperTextEnabled = true
-            passwordLayout.error = getString(R.string.enterPassword)
-        }else{
-            passwordLayout.error = null
+        binding.apply {
+            if (binding.password.length() == 0){
+                binding.passwordLayout.isHelperTextEnabled = true
+                binding.passwordLayout.error = getString(R.string.enterPassword)
+            }else{
+                binding.passwordLayout.error = null
+            }
+
+            invalidateAll() //refresh UI
         }
     }
 
     private fun checkUsername() {
-        if (usernameTxt.length() == 0){
-            usernameLayout.isHelperTextEnabled = true
-            usernameLayout.error = getString(R.string.enterUsername)
-        }else if(usernameTxt.text!!.contains(" ")){
-            usernameLayout.isHelperTextEnabled = true
-            usernameLayout.error = getString(R.string.usernameWithSpace)
-        }else{
-            usernameLayout.error = null
+        binding.apply {
+            if (binding.username.length() == 0){
+                binding.usernameLayout.isHelperTextEnabled = true
+                binding.usernameLayout.error = getString(R.string.enterUsername)
+            }else if(binding.username.text!!.contains(" ")){
+                binding.usernameLayout.isHelperTextEnabled = true
+                binding.usernameLayout.error = getString(R.string.usernameWithSpace)
+            }else{
+                binding.usernameLayout.error = null
+            }
+
+            invalidateAll()
         }
     }
 }
