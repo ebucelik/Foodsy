4ba115b42b6fb500533b9b803d11df457df2eda4
Ebu Bekir Celik <ebu.celik@stud.fh-campuswien.ac.at>
2020-12-25 20:33:59 +0100

    Offer Meal with image picker finished. Account View is in development.

--- app/src/main/java/ebucelik/keepeasy/foodsy/home/AccountFragment.kt
+++ app/src/main/java/ebucelik/keepeasy/foodsy/home/AccountFragment.kt
@@ -6,7 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ebucelik.keepeasy.foodsy.R
+import ebucelik.keepeasy.foodsy.databinding.FragmentAccountBinding
+import kotlin.math.round
 
 class AccountFragment : Fragment(R.layout.fragment_account) {
+
+    private lateinit var binding: FragmentAccountBinding
+    private val review = 3.4
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentAccountBinding.bind(view)
+
+        setReview()
+    }
+
+    private fun setReview(){
+        val reviewInt = round(review).toInt()
+
+        if(reviewInt >= 1){
+            for (i in 0 until reviewInt){
+                when(i){
+                    0 -> binding.rate1.setImageResource(R.drawable.ic_round_star_rate_24_gold)
+                    1 -> binding.rate2.setImageResource(R.drawable.ic_round_star_rate_24_gold)
+                    2 -> binding.rate3.setImageResource(R.drawable.ic_round_star_rate_24_gold)
+                    3 -> binding.rate4.setImageResource(R.drawable.ic_round_star_rate_24_gold)
+                    4 -> binding.rate5.setImageResource(R.drawable.ic_round_star_rate_24_gold)
+                }
+            }
+        }
+    }
 
 }
--- app/src/main/java/ebucelik/keepeasy/foodsy/home/SellFragment.kt
+++ app/src/main/java/ebucelik/keepeasy/foodsy/home/SellFragment.kt
@@ -1,12 +1,42 @@
 package ebucelik.keepeasy.foodsy.home
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ebucelik.keepeasy.foodsy.R
+import ebucelik.keepeasy.foodsy.databinding.FragmentSellBinding
 
 class SellFragment : Fragment(R.layout.fragment_sell) {
 
+    private val REQUEST_SELECT_IMAGE_IN_ALBUM = 1
+    private lateinit var binding: FragmentSellBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentSellBinding.bind(view)
+
+        binding.mealImage.setOnClickListener {
+            selectImageFromGallery()
+        }
+    }
+
+    private fun selectImageFromGallery(){
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == RESULT_OK && requestCode == REQUEST_SELECT_IMAGE_IN_ALBUM){
+            val imageURI = data?.data
+            binding.mealImage.setImageURI(imageURI)
+        }
+    }
 }
